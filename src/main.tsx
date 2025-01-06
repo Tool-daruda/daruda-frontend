5fb5734b7702baee33228cddc304d0998ba8ff0d
imddoy <imddoy@duksung.ac.kr>
2025-01-06 16:13:45 +0900

    Feat/#13: theme 타입 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@styles/GlobalStyles';
-import theme from '@styles/theme.ts';
+import theme from '@styles/theme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
