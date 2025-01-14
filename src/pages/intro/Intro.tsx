d0d9465ab9355980b88f07f0dd8ac32581838e4f
imddoy <imddoy@duksung.ac.kr>
2025-01-14 22:01:11 +0900

    Remove/#41: 테스트 컴포넌트 삭제

--- src/pages/intro/Intro.tsx
+++ src/pages/intro/Intro.tsx
@@ -1,16 +1,5 @@
-import MyToolCard from '@pages/myPage/components/toolCard/MyToolCard';
-
 const Intro = () => {
-  return (
-    <MyToolCard
-      toolLogo=""
-      toolNameMain="ChatGPT"
-      keyWordList={[
-        { keyWordId: 3, keyWordName: '디자인' },
-        { keyWordId: 4, keyWordName: '프로토타입' },
-      ]}
-    />
-  );
+  return <></>;
 };
 
 export default Intro;
--- src/pages/myPage/MyInfoPage.tsx
+++ src/pages/myPage/MyInfoPage.tsx
@@ -1,19 +1,5 @@
-import MyToolCard from './components/toolCard/MyToolCard';
-
 const MyInfoPage = () => {
-  return (
-    <>
-      {' '}
-      <MyToolCard
-        toolLogo=""
-        toolNameMain="ChatGPT"
-        keyWordList={[
-          { keyWordId: 3, keyWordName: '디자인' },
-          { keyWordId: 4, keyWordName: '프로토타입' },
-        ]}
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default MyInfoPage;
