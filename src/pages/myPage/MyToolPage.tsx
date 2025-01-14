6377aa387f5436c567ba7b82160509d5827ba0a7
imddoy <imddoy@duksung.ac.kr>
2025-01-15 03:15:42 +0900

    Feat/#41: tool 없을 때 뷰 추가

--- src/pages/myPage/MyToolPage.tsx
+++ src/pages/myPage/MyToolPage.tsx
@@ -1,11 +1,24 @@
+import Spacing from '@components/spacing/Spacing';
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 
 import MyToolCard from './components/toolCard/MyToolCard';
 import { favoriteToolList } from './constants/favoriteToolList';
 
+interface keyWord {
+  keyWordId: number;
+  keyWordName: string;
+}
+
+interface Tool {
+  toolId: number;
+  toolLogo: string;
+  toolNameMain: string;
+  keyWordList: keyWord[];
+}
+
 const MyToolPage = () => {
-  const [toolList, setToolList] = useState(favoriteToolList);
+  const [toolList, setToolList] = useState<Tool[]>(favoriteToolList);
 
   // 추후에 API 연결을 위해 useState를 사용하기 위해 set함수를 임의로 넣었습니다!!!
   // API 연결할 때 삭제하겠습니다.
@@ -15,16 +28,30 @@ const MyToolPage = () => {
 
   return (
     <S.MyToolWrapper>
-      <S.MyToolContainer>
-        {toolList.map((tool) => (
-          <MyToolCard
-            key={tool.toolId}
-            toolLogo={tool.toolLogo}
-            toolNameMain={tool.toolNameMain}
-            keyWordList={tool.keyWordList}
+      {toolList.length > 0 ? (
+        <S.MyToolContainer>
+          {toolList.map((tool) => (
+            <MyToolCard
+              key={tool.toolId}
+              toolLogo={tool.toolLogo}
+              toolNameMain={tool.toolNameMain}
+              keyWordList={tool.keyWordList}
+            />
+          ))}
+        </S.MyToolContainer>
+      ) : (
+        <S.NonTool>
+          {/* TODO: 이미지 갈아끼우기 */}
+          <img
+            src="https://mblogthumb-phinf.pstatic.net/MjAxOTEwMTFfNjEg/MDAxNTcwNzg1ODM3Nzc0.zxDXm20VlPdQv8GQi9LWOdPwkqoBdiEmf8aBTWTsPF8g.FqMQTiF6ufydkQxrLBgET3kNYAyyKGJTWTyi1qd1-_Ag.PNG.kkson50/sample_images_01.png?type=w800"
+            alt=""
           />
-        ))}
-      </S.MyToolContainer>
+          <Spacing size="4.2" />
+          <p>관심있는 툴이 없어요</p>
+          <Spacing size="1" />
+          <p>북마크를 통해 관심있는 툴을 저장해보세요</p>
+        </S.NonTool>
+      )}
     </S.MyToolWrapper>
   );
 };
@@ -65,5 +92,24 @@ const S = {
     gap: 1.2rem;
     width: 81.3rem;
     margin: 1.2rem 0;
+  `,
+  NonTool: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: inheriht;
+    margin-left: -3.6rem;
+
+    & p:first-of-type {
+      color: ${({ theme }) => theme.colors.gray1};
+
+      ${({ theme }) => theme.fonts.body_20_b};
+    }
+
+    & p:last-of-type {
+      color: ${({ theme }) => theme.colors.gray2};
+
+      ${({ theme }) => theme.fonts.caption_14_m};
+    }
   `,
 };
