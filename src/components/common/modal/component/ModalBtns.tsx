68a9580b8825e4c2d7f9c7e06d319e0e6a7a367e
imddoy <imddoy@duksung.ac.kr>
2025-01-17 04:18:07 +0900

    Feat/#72: 모달 수정

--- src/components/common/modal/component/ModalBtns.tsx
+++ src/components/common/modal/component/ModalBtns.tsx
@@ -18,10 +18,10 @@ const DoubleBtns = ({
 }: ModalBtnsProps) => {
   return (
     <S.ModalBtns>
-      <S.ModalBtn $isPrimary={false} order={isPrimaryRight ? 0 : 1} onClick={handleSecondClose}>
+      <S.ModalBtn $isPrimary={true} order={isPrimaryRight ? 1 : 0} onClick={handleSecondClose}>
         {primaryBtnContent}
       </S.ModalBtn>
-      <S.ModalBtn $isPrimary={true} order={isPrimaryRight ? 1 : 0} onClick={handleClose}>
+      <S.ModalBtn $isPrimary={false} order={isPrimaryRight ? 0 : 1} onClick={handleClose}>
         {secondaryBtnContent}
       </S.ModalBtn>
     </S.ModalBtns>
