0c8e08e84f7b766faa5c138a78fb33df4bf5cb05
imddoy <imddoy@duksung.ac.kr>
2025-01-17 16:16:03 +0900

    Feat/#78: 툴 리스트 이동 버튼 추가

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,10 +1,13 @@
 import { Error } from '@assets/svgs';
 import CircleButton from '@components/button/circleButton/CircleButton';
 import Spacing from '@components/spacing/Spacing';
+import { useNavigate } from 'react-router-dom';
 
 import * as S from './NotFound.styled';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
       <S.Left>
@@ -12,7 +15,9 @@ const NotFound = () => {
         <Spacing size="0.4" />
         <S.Content> 다루다를 다시 시작해주세요</S.Content>
         <Spacing size="4" />
-        <CircleButton size="mini">다시 시작하기</CircleButton>
+        <CircleButton size="mini" onClick={() => navigate('/toolist')}>
+          다시 시작하기
+        </CircleButton>
         <Spacing size="18" />
         <span>오류코드 : 404</span>
       </S.Left>
