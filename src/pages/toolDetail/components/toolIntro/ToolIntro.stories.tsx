45614cc10b4cb5f36925a8bbd19b9225e69f0eb0
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-01-19 21:39:25 +0900

    Fix/#68: storybook 데이터 연결

--- src/pages/toolDetail/components/toolIntro/ToolIntro.stories.tsx
+++ src/pages/toolDetail/components/toolIntro/ToolIntro.stories.tsx
@@ -1,4 +1,4 @@
-import { TOOL_DESCRIPTIONS } from '@pages/toolDetail/mocks/toolData';
+import { DETAIL_RESPONSE } from '@pages/toolDetail/mocks/toolData';
 import type { Meta, StoryFn } from '@storybook/react';
 
 import ToolIntro from './ToolIntro';
@@ -13,7 +13,7 @@ export default {
     toolKey: {
       control: {
         type: 'select',
-        options: Object.keys(TOOL_DESCRIPTIONS),
+        options: Object.keys(DETAIL_RESPONSE),
       },
       description: '툴의 키 (데이터를 선택하는 데 사용)',
     },
@@ -28,29 +28,32 @@ export default {
 
 const Template: StoryFn<typeof ToolIntro> = (args) => <ToolIntro {...args} />;
 
-export const Slack = Template.bind({});
-Slack.args = {
-  toolKey: 'slack',
-  toolImage:
+export const Anaconda = Template.bind({});
+Anaconda.args = {
+  toolImage: [
     'https://www.uctoday.com/wp-content/uploads/2023/08/Slack-Launches-Redesign-to-Boost-User-Productivity.jpg',
+  ],
+  activeTool: 'Anaconda',
+  description:
+    'Anaconda는 데이터 과학, 머신러닝, 인공지능, 분석 작업을 효율적으로 수행할 수 있도록 지원하는 오픈 소스 기반의 데이터 과학 플랫폼입니다. 이 플랫폼은 Python과 R 프로그래밍 언어를 기반으로 하며, 복잡한 데이터 분석과 대규모 데이터 처리를 단순화하는 도구와 환경을 제공합니다.',
 };
 
 export const Notion = Template.bind({});
 Notion.args = {
-  toolKey: 'notion',
-  toolImage:
+  toolImage: [
     'https://cdn.inflearn.com/public/courses/324181/course_cover/4f20d4ce-80a2-4334-a85b-429a55ce4bad/boan_notion.png',
+  ],
+  activeTool: 'Notion',
+  description:
+    'Notion는 데이터 과학, 머신러닝, 인공지능, 분석 작업을 효율적으로 수행할 수 있도록 지원하는 오픈 소스 기반의 데이터 과학 플랫폼입니다. 이 플랫폼은 Python과 R 프로그래밍 언어를 기반으로 하며, 복잡한 데이터 분석과 대규모 데이터 처리를 단순화하는 도구와 환경을 제공합니다.',
 };
 
 export const Jira = Template.bind({});
 Jira.args = {
-  toolKey: 'jira',
-  toolImage:
+  toolImage: [
     'https://wac-cdn.atlassian.com/dam/jcr:d42e8c86-a14b-4ec0-8753-d30c65dce581/Dashbaords-view.png?cdnVersion=2510',
-};
-
-export const WithoutImage = Template.bind({});
-WithoutImage.args = {
-  toolKey: 'slack',
-  toolImage: '',
+  ],
+  activeTool: 'Jira',
+  description:
+    'Notion는 데이터 과학, 머신러닝, 인공지능, 분석 작업을 효율적으로 수행할 수 있도록 지원하는 오픈 소스 기반의 데이터 과학 플랫폼입니다. 이 플랫폼은 Python과 R 프로그래밍 언어를 기반으로 하며, 복잡한 데이터 분석과 대규모 데이터 처리를 단순화하는 도구와 환경을 제공합니다.',
 };
