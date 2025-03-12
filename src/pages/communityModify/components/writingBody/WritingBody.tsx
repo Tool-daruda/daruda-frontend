3cff732105ff145a52d261c62aee4d6087f7f214
imddoy <imddoy@duksung.ac.kr>
2025-03-12 11:28:44 +0900

    Chore/#189: 필요없는 import 삭제

--- src/pages/communityModify/components/writingBody/WritingBody.tsx
+++ src/pages/communityModify/components/writingBody/WritingBody.tsx
@@ -1,3 +1,4 @@
+import Toast from '@components/toast/Toast';
 import React, { useState } from 'react';
 
 import * as S from './WritingBody.styled';
@@ -5,15 +6,20 @@ import * as S from './WritingBody.styled';
 interface WritingBodyProps {
   originBody: string;
   setBody: (text: string) => void;
+  onImageUpload: (files: File[]) => void;
+  images: File[];
 }
 
 const MAX_CHAR_LIMIT = 10000;
+const MAX_IMG_SIZE_LIMIT = 7;
+const MAX_IMG_COUNT_LIMIT = 5;
 
-const WritingBody = ({ originBody, setBody }: WritingBodyProps) => {
+const WritingBody = ({ originBody, setBody, onImageUpload, images }: WritingBodyProps) => {
   const [text, setText] = useState(originBody);
   const [triggerShake, setTriggerShake] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
   const isExceedingLimit = text.length >= MAX_CHAR_LIMIT;
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inputText = e.target.value;
@@ -35,6 +41,34 @@ const WritingBody = ({ originBody, setBody }: WritingBodyProps) => {
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
 
+  const handleImagePaste = (e: React.ClipboardEvent) => {
+    const items = e.clipboardData.items;
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].type.startsWith('image/')) {
+        const blob = items[i].getAsFile();
+        if (blob) {
+          if (blob.size > MAX_IMG_SIZE_LIMIT * 1024 * 1024) {
+            setIsVisible(true);
+            setTimeout(() => {
+              setIsVisible(false);
+            }, 3000);
+            return;
+          }
+          if (images.length === MAX_IMG_COUNT_LIMIT) {
+            setIsVisible(true);
+            setTimeout(() => {
+              setIsVisible(false);
+            }, 3000);
+            return;
+          }
+          onImageUpload([blob]);
+        }
+        break;
+      }
+    }
+  };
+
   return (
     <S.Container
       isActive={isFocused}
@@ -49,10 +83,16 @@ const WritingBody = ({ originBody, setBody }: WritingBodyProps) => {
         onChange={handleTextChange}
         onFocus={handleFocus}
         onBlur={handleBlur}
+        onPaste={handleImagePaste}
       />
       <S.CharCount isExceedingLimit={isExceedingLimit}>
         {text.length} / {MAX_CHAR_LIMIT}
       </S.CharCount>
+      {isVisible && (
+        <Toast isVisible={isVisible} isWarning>
+          이미지 업로드 용량은 한장 당 최대 7MB 입니다.
+        </Toast>
+      )}
     </S.Container>
   );
 };
