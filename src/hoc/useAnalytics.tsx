78b7bd33d9255cfadc2fce95179de3ae88cd35a9
goeun kim <rlarhdms6828@naver.com>
2025-04-01 09:53:52 +0900

    feat/#204: mixpanel init setting

---
+++ src/hoc/useAnalytics.tsx
@@ -0,0 +1,36 @@
+import mixpanel from 'mixpanel-browser';
+import React, { createContext, useContext, useEffect } from 'react';
+
+declare module 'mixpanel-browser' {
+  interface Config {
+    autocapture?: boolean;
+  }
+}
+
+type AnalyticsContextProps = {
+  trackEvent: <T extends Record<string, unknown>>(eventName: string, eventProperties?: T) => void;
+};
+
+const AnalyticsContext = createContext<AnalyticsContextProps | undefined>(undefined);
+
+const AnalyticsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const trackEvent = <T extends Record<string, unknown>>(eventName: string, eventProperties?: T) => {
+    mixpanel.track(eventName, eventProperties);
+  };
+
+  useEffect(() => {
+    mixpanel.init(import.meta.env.VITE_MIXPANEL_KEY, {
+      debug: import.meta.env.MODE === 'development',
+    });
+  }, []);
+
+  return <AnalyticsContext.Provider value={{ trackEvent }}>{children}</AnalyticsContext.Provider>;
+};
+
+export const useAnalytics = () => {
+  const context = useContext(AnalyticsContext);
+  if (!context) throw new Error('AnalyticsProvider 안에서만 이벤트를 트랙할 수 있습니다');
+  return context;
+};
+
+export default AnalyticsProvider;
