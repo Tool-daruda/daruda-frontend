3c048e7bb928a5062fbd0c505933f2ff17b62a05
imddoy <imddoy@duksung.ac.kr>
2025-06-02 14:17:01 +0900

    Feat: 툴 검색결과 렌더링 구현 [#222]

--- src/pages/search/Search.styled.ts
+++ src/pages/search/Search.styled.ts
@@ -12,7 +12,7 @@ export const SearchBox = styled.div`
   flex-direction: column;
   gap: 4rem;
   align-items: center;
-  width: 86rem;
+  width: 104.6rem;
   margin: 2.4rem;
   padding: 4rem 9.3rem;
 
@@ -31,6 +31,48 @@ export const SearchResult = styled.section`
   h2 {
     ${({ theme }) => theme.fonts.body_20_b};
     color: ${({ theme }) => theme.colors.gray1};
+  }
+`;
+
+export const ToolCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+`;
+
+export const ToolCardWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  width: 42rem;
+`;
+
+export const Button = styled.button`
+  width: fit-content;
+
+  ${({ theme }) => theme.fonts.body_16_m};
+  color: ${({ theme }) => theme.colors.iris1};
+`;
+
+export const Toggle = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  ${({ theme }) => theme.fonts.body_16_r};
+  color: ${({ theme }) => theme.colors.gray2};
+  text-align: center;
+
+  svg {
+    transform: ${({ $isOpen }) => ($isOpen ? 'rotate(0deg)' : 'rotate(180deg)')};
+
+    transition: 0.5s;
+  }
+
+  path {
+    fill: ${({ theme }) => theme.colors.gray2};
   }
 `;
 
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,13 +1,24 @@
-import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
+import { toolSearchResult } from './mock/response';
 import * as S from './Search.styled';
+import { Tool } from '@apis/tool';
+import { IcChevron } from '@assets/svgs';
 import Spacing from '@components/spacing/Spacing';
+import ToolCard from '@components/toolCard/ToolCard';
 import TopBanner from '@pages/toolList/components/topBanner/TopBanner';
 
 const Search = () => {
   const [searchParams] = useSearchParams();
-
+  const [toolResult, setToolResult] = useState<Tool[]>([]);
+  const navigate = useNavigate();
   const searchKeyword = searchParams.get('keyword');
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setToolResult(toolSearchResult.slice(0, 2));
+  }, []);
 
   return (
     <S.SearchWrapper>
@@ -17,7 +28,35 @@ const Search = () => {
           <h1>{searchKeyword ? `"${searchKeyword}"에 대한 검색결과입니다.` : '검색어를 입력해주세요.'}</h1>
           <h2>툴 리스트</h2>
           <Spacing size="2" />
+          <S.ToolCardContainer>
+            {toolResult?.map((tool) => (
+              <S.ToolCardWrapper key={tool.toolId}>
+                <ToolCard tool={tool} />
+                <S.Button
+                  onClick={() => {
+                    sessionStorage.setItem(
+                      'originTool',
+                      JSON.stringify({ toolId: tool.toolId, toolLogo: tool.toolLogo, toolName: tool.toolName }),
+                    );
+                    navigate('/community');
+                  }}
+                >
+                  관련 글 모아보기
+                </S.Button>
+              </S.ToolCardWrapper>
+            ))}
+          </S.ToolCardContainer>
         </S.SearchResult>
+        <S.Toggle
+          onClick={() => {
+            setToolResult(isOpen ? toolSearchResult.slice(0, 2) : toolSearchResult);
+            setIsOpen((prev) => !prev);
+          }}
+          $isOpen={isOpen}
+        >
+          {isOpen ? '검색결과 접기' : '검색결과 펼치기'}
+          <IcChevron />
+        </S.Toggle>
         <S.Divider />
         <S.SearchResult>
           <h2>커뮤니티 전체</h2>
