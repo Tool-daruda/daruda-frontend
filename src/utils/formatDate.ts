cf08ede118badcffd6bb3cbf049d54f700325d24
goeun kim <rlarhdms6828@naver.com>
2025-07-07 00:08:19 +0900

    Chore: date format 형식 변경 [#224]

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,7 @@
 export const formatToMonthDay = (dateString: Date): string => {
   const date = new Date(dateString);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1);
+  const day = String(date.getDate());
 
   return `${month}월 ${day}일`;
 };
